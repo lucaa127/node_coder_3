d8e92bd2ce2444614ba063ccbfe18d399794cd75
Luca Arce <luca.a127@gmail.com>
2022-09-12 22:37:02 -0300

    knex-mssql-test

---
+++ clase16/crud-knex-mysql/src/batch/createTables.js
@@ -0,0 +1,22 @@
+import knex from 'knex';
+import { config } from '../utils/config.js';
+
+const knexCli = knex(config.db);
+
+knexCli.schema.dropTableIfExists('autos')
+    .then(()=>{
+        knexCli.schema.createTable('autos', table => {
+            table.increments('id').primary();
+            table.string('marca', 50).notNullable();
+            table.string('modelo', 50).notNullable();
+        })
+            .then(()=> console.log("Tabla creada"))
+            .catch(err=> {
+                console.log(err); 
+                throw err;
+            })
+            .finally(()=>{
+                knexCli.destroy();
+            });
+    });
+
---
+++ clase16/crud-knex-mysql/src/utils/config.js
@@ -0,0 +1,71 @@
+
+export const config = {
+    db: {
+        client: 'mssql',
+        connection: {
+            host: 'localhost',
+            user: 'sa',
+            password: 'Lleave11',
+            database: 'test',
+            options: {
+                port: 63598
+              }
+        }
+    }
+}
+
+
+
+
+
+// export const config = {
+//    db: {
+//     client: 'mssql',
+//     connection: {
+//         // no tcp:
+//         server: 'LOCALHOST/SQLEXPRESS',
+//         user: 'sa',
+//         password: 'Lleave11',
+//         database: 'test',
+//         port: 1433,
+//         options: { requestTimeout: 350000, encrypt: true },
+//     },
+//     pool: {
+//         min: 0,
+//         max: 15,
+//      }
+//     }
+// }
+
+
+
+// export const config = {
+//    db: {
+//     "user": 'sa',
+//     "client": 'mssql',
+//     "password": 'Lleave11',
+//     "server": 'localhost',
+//     "database": 'test',
+//     "port": 1433, // make sure to change port
+    
+//     "dialectOptions": {
+//         "instanceName": "SQLEXPRESS"
+//     }
+// }
+// };
+
+
+// export const config = {
+//     db: {
+//         client: 'mssql',
+//         connection: {
+//             host: '127.0.0.1/SQLEXPRESS',
+//             user: 'sa',
+//             password: 'Lleave11',
+//             database: 'test',
+//             options: {
+//                 port: 1433,
+//               },
+//         }
+//     }
+// }
